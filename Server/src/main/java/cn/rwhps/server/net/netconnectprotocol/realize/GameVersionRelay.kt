11d63dbd0ee1d588768be081266150f56dba4733
Ілля Махнотов <70032794+Tstis@users.noreply.github.com>
2022-04-23 15:20:45 +0300

    fix

--- Server/src/main/java/cn/rwhps/server/net/netconnectprotocol/realize/GameVersionRelay.kt
+++ Server/src/main/java/cn/rwhps/server/net/netconnectprotocol/realize/GameVersionRelay.kt
@@ -307,13 +307,13 @@ open class GameVersionRelay(connectionAgreement: ConnectionAgreement) : Abstract
             // List OPEN
             o.writeBoolean(false)
             o.writeBoolean(true)
-            o.writeString("{{RW-HPS }}.Room ID : " + relay!!.id)
+            o.writeString("{{ УІД >>> s.vangers.tk >>> ukr" + relay!!.id + " }}")
             // 多播
             o.writeBoolean(false)
             sendPacket(o.createPacket(PacketType.FORWARD_HOST_SET)) //+108+140
             //getRelayT4(Data.localeUtil.getinput("relay.server.admin.connect",relay.getId()));
-            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay.server.admin.connect", relay!!.id), "ADMIN", 5))
-            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay", relay!!.id), "ADMIN", 5))
+            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay.server.admin.connect", relay!!.id), "РЕКЛАМА", 3))
+            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay", relay!!.id), "СЕРВЕР", -1))
             //ping();
 
             //debug(name)
@@ -393,7 +393,7 @@ open class GameVersionRelay(connectionAgreement: ConnectionAgreement) : Abstract
             o1.writeBytes(cachePacket!!.bytes)
             relay!!.admin!!.sendPacket(o1.createPacket(PacketType.PACKET_FORWARD_CLIENT_FROM))
             connectionAgreement.add(relay!!.groupNet)
-            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay", relay!!.id), "ADMIN", 5))
+            sendPacket(NetStaticData.RwHps.abstractNetPacket.getChatMessagePacket(Data.i18NBundle.getinput("relay", relay!!.id), "СЕРВЕР", -1))
         } catch (e: IOException) {
             e.printStackTrace()
         } finally {
@@ -552,12 +552,12 @@ open class GameVersionRelay(connectionAgreement: ConnectionAgreement) : Abstract
     private fun idCustom(inId: String) {
         var id = inId
         if (id.isEmpty()) {
-            sendRelayServerType(Data.i18NBundle.getinput("relay.server.no", "空"))
+            sendRelayServerType(Data.i18NBundle.getinput("relay.server.no", "-0"))
             return
         }
 
-        if ("R".equals(id[0].toString(), ignoreCase = true)) {
-            id = id.substring(1)
+        if ("ukr".equals(id[0].toString(), ignoreCase = true)) {
+            id = id.substring(3)
         } else if ("C".equals(id[0].toString(), ignoreCase = true)) {
             id = id.substring(1)
             val customID = splitNumbers(id)
@@ -582,9 +582,9 @@ open class GameVersionRelay(connectionAgreement: ConnectionAgreement) : Abstract
         }
 
         if (IsUtil.notIsBlank(id)) {
-            if ("new".equals(id, ignoreCase = true)) {
+            if ("new".equals(id, ignoreCase = true) || "нова".equals(id, ignoreCase = true) || "н".equals(id, ignoreCase = true)) {
                 newRelayId(false)
-            } else if ("mod".equals(id, ignoreCase = true) || "mods".equals(id, ignoreCase = true)) {
+            } else if ("mod".equals(id, ignoreCase = true) || "mods".equals(id, ignoreCase = true) || "мод".equals(id, ignoreCase = true) || "моди".equals(id, ignoreCase = true) || "модифікація".equals(id, ignoreCase = true) || "модифікації".equals(id, ignoreCase = true) || "м".equals(id, ignoreCase = true)) {
                 newRelayId(true)
             } else {
                 try {
